a78e0b15ccc5ce76af249cbc7c9155228086770d
rahman.ibrahim <rahman.ibrahim@hypaship.com>
2022-04-06 01:11:39 +0800

    Updated a bit to connect

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -3,12 +3,12 @@ import { Web3Provider } from '@ethersproject/providers'
 import { InjectedConnector } from '@web3-react/injected-connector'
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 import { WalletLinkConnector } from '@web3-react/walletlink-connector'
-import { BscConnector } from '@binance-chain/bsc-connector'
+// import { BscConnector } from '@binance-chain/bsc-connector'
 import { NetworkConnector } from './NetworkConnector'
 
 const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
 
-export const NETWORK_CHAIN_ID: number = parseInt(process.env.REACT_APP_CHAIN_ID ?? '56')
+export const NETWORK_CHAIN_ID: number = parseInt(process.env.REACT_APP_CHAIN_ID ?? '137')
 
 if (typeof NETWORK_URL === 'undefined') {
   throw new Error(`REACT_APP_NETWORK_URL must be a defined environment variable`)
@@ -25,10 +25,10 @@ export function getNetworkLibrary(): Web3Provider {
 }
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [56, 97, 80001, 137],
+  supportedChainIds: [80001, 137],
 })
 
-export const bscConnector = new BscConnector({ supportedChainIds: [56, 80001] })
+// export const bscConnector = new BscConnector({ supportedChainIds: [56, 80001] })
 
 // mainnet only
 export const walletconnect = new WalletConnectConnector({
