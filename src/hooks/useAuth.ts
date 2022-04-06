c19b442c6474f28248e4874fb086d7bd9f7f4d05
rahman.ibrahim <rahman.ibrahim@hypaship.com>
2022-04-06 12:38:15 +0800

    skipped checks

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -22,11 +22,7 @@ const useAuth = () => {
     if (connector) {
       activate(connector, async (error: Error) => {
         window.localStorage.removeItem(connectorLocalStorageKey)
-        if (error instanceof UnsupportedChainIdError) {
-          toastError('Unsupported Chain Id', 'Unsupported Chain Id Error. Check your chain Id.')
-        } else if (error instanceof NoEthereumProviderError || error instanceof NoBscProviderError) {
-          toastError('Provider Error', 'No provider was found')
-        } else if (
+        if (
           error instanceof UserRejectedRequestErrorInjected ||
           error instanceof UserRejectedRequestErrorWalletConnect
         ) {
